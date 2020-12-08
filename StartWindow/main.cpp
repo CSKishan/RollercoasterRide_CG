f555d4b443366e285355dd63765cba7cb8d490f4
Kishan <kishanland@gmail.com>
2020-12-08 22:25:06 +0530

    start Window alignment

--- StartWindow/main.cpp
+++ StartWindow/main.cpp
@@ -12,7 +12,7 @@
  * using the + and - keys.
  */
 
- #include<windows.h>
+#include<windows.h>
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -23,10 +23,8 @@
 
 void init()
 {
-    glClearColor(0.5,0,0.5,0.5);
-
+    glClearColor(0.0,0.0,0.0,0.0);
     glMatrixMode(GL_PROJECTION);
-
     glOrtho(0,50,0,50,0,10);
 
 }
@@ -46,101 +44,45 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glColor3f(0.0,1.0,0.0);
-
     glRasterPos2i(15,40);
-
-
-
     glRasterPos3f(19.5,40,0);
-
     Display_on_screen("ROLLERCOASTER RIDE");
 
-
-
-    glRasterPos3f(12,26,0);
-
-    Display_on_screen("Keertesh S");
-
-
-
-    glRasterPos3f(21,26,0);
-
-    Display_on_screen("<-->");
-
-
-
-    glRasterPos3f(26,26,0);
-
+    glRasterPos3f(7,26,0);
+    Display_on_screen("Keerthesh S");
+    glRasterPos3f(34,26,0);
     Display_on_screen("4NM17CS087");
 
-
-
-    glRasterPos3f(12,24,0);
-
+    glRasterPos3f(7,22,0);
     Display_on_screen("Kiran Mahadev Giraddi");
-
-
-
-    glRasterPos3f(21,24,0);
-
-    Display_on_screen("<-->");
-
-
-
-    glRasterPos3f(26,24,0);
-
+    glRasterPos3f(34,22,0);
     Display_on_screen("4NM17CS088");
 
-
-
-    glRasterPos3f(12,22,0);
-
+    glRasterPos3f(7,18,0);
     Display_on_screen("Kirthi Puthran");
-
-
-
-    glRasterPos3f(21,22,0);
-
-    Display_on_screen("<-->");
-
-
-
-    glRasterPos3f(26,22,0);
-
+    glRasterPos3f(34,18,0);
     Display_on_screen("4NM17CS089");
 
-
-
-    glRasterPos3f(12,20,0);
-
+    glRasterPos3f(7,14,0);
     Display_on_screen("Kishan");
-
-
-
-    glRasterPos3f(21,20,0);
-
-    Display_on_screen("<-->");
-
-
-
-    glRasterPos3f(26,20,0);
-
+    glRasterPos3f(34,14,0);
     Display_on_screen("4NM17CS090");
 
 
-
+    // top line
     glColor3f(1.0,0.0,0.0);
-
     glLineWidth(5);
-
     glBegin(GL_LINES);
+    glVertex2i(17.5,42.7);
+    glVertex2i(31.5,42.7);
 
-    glVertex2i(16,39);
-
-    glVertex2i(28,39);
-
+    // bottom line
+    glColor3f(1.0,0.0,0.0);
+    glLineWidth(5);
+    glBegin(GL_LINES);
+    glVertex2i(17.5,39);
+    glVertex2i(31.5,39);
     glEnd();
-
     glFlush();
 
 }
@@ -149,21 +91,12 @@ void display()
 
 int main(int argc, char **argv)
 {
-
     glutInit(&argc,argv);
-
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB);
-
     glutInitWindowPosition(1,1);
-
     glutInitWindowSize(1400, 950);
-
     glutCreateWindow("CG PROJECT");
-
     init();
-
     glutDisplayFunc(display);
-
     glutMainLoop();
-
 }
